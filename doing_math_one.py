c1bb2508dd96208e7b5c9c329d1504f4d2cbbcc8
Ubuntu <ubuntu@ip-172-31-29-95.ec2.internal>
2019-09-13 18:33:21 +0000

    initial message

---
+++ doing_math_one.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# created by: Izza Khalid
+# created on: September 2019
+# This program calculates the area and perimeter of a rectangle with dimensions
+#   5cm x 3cm
+
+
+def main():
+    print("If the rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
